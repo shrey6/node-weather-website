651edc2a229a4dea7e3975f36a04f83a18402ccc
unknown <shreyagarwal611@gmail.com>
2020-05-24 14:03:38 +0530

    timepass

--- public/js/app.js
+++ public/js/app.js
@@ -2,19 +2,22 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location = search.value
-    messageOne.textContent = 'Loading!!!!'
+    messageOne.textContent = 'ruk zara load hone da'
     messageTwo.textContent=""
+    messageThree.textContent=""
     fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent = data.error
         }
         else{
-            messageOne.textContent = data.location
+            messageOne.textContent = "keval apni location dekhiyo kissi ladki ki nhi  "+ data.location
             messageTwo.textContent = data.forecast
+            messageThree.textContent = "tapmaan dekh bhi liya bahot tez hora hai!!!"
         }
     })
 })
