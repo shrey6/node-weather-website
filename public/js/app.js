a62bfd51e2d79f59a65a165e22cf66db077e3ea4
unknown <shreyagarwal611@gmail.com>
2020-05-24 12:37:55 +0530

    remove comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,18 @@
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-// messageOne.textContent=''
-
-
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
-    // console.log('testing!!!')
     const location = search.value
     messageOne.textContent = 'Loading!!!!'
     messageTwo.textContent=""
     fetch('http://localhost:3000/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
-            // console.log(data.error)
             messageOne.textContent = data.error
         }
         else{
-            // console.log(data.location)
-            // console.log(data.forecast)
             messageOne.textContent = data.location
             messageTwo.textContent = data.forecast
         }
